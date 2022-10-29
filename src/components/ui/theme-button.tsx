d0664af40e095219eb4a3c3b6239042c428c8258
skrambl <55457634+MattEagle95@users.noreply.github.com>
2022-10-29 14:46:07 +0200

    ----

--- src/components/ui/theme-button.tsx
+++ src/components/ui/theme-button.tsx
@@ -1,14 +1,18 @@
 import { useTheme } from 'next-themes';
 import useTranslation from 'next-translate/useTranslation';
 import { Moon, Sun } from 'phosphor-react';
-import { forwardRef } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 
 import { Button, ButtonProps } from './button';
 
 // eslint-disable-next-line react/display-name
 export const ThemeButton = forwardRef((props: ButtonProps, ref: any) => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
   const { t } = useTranslation();
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
 
   const toggleTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
